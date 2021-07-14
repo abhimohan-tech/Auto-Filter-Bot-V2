8571db47f2c169bdbf65d142acf90ea910743487
abhimohan-tech <85671955+abhimohan-tech@users.noreply.github.com>
2021-07-14 11:10:26 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,64 +1,66 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
+    START_MSG = """ <b>Hi {} How are you?
 
-You can call this as an Auto Filter Bot if you like :D
+Am Just A Advance Auto Filter Bot Only For <b>FF Group</b>
 
-This is Version 2 of Auto Filter Bot
+You Can't add me to your Group 😉😁
 
-Bot gives button link to files in connected channels on query !
 
-No need to add filters for your files or movies from now on!
+
+
+
+
 
 For more click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
+<b>
+ആരാടാ നീ Help ചോദിച്ച് വന്നിരിക്കുന്നു </b>
 
 
 
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creator :</b> @rocky_boy01
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
